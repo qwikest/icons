83f7bc5ef7ceefd9175330f42e0584fc5132f341
Niklas <niklaspor@gmail.com>
2023-05-06 21:22:44 +0200

    add box icons

---
+++ generate/packs/boxicons.ts
@@ -0,0 +1,38 @@
+import glob from "fast-glob";
+import { definePack } from "../define-pack";
+import { extractor } from "../extractor";
+
+const extractRegex = /^.*\/(?<variant>.+?)\/(?<name>.+?)\.svg/;
+function boxIconExtract(path: string) {
+  const baseExtractor = extractor(extractRegex, { variant: "regular" });
+  const { name, variant } = baseExtractor(path);
+
+  return {
+    name,
+    variant: variant === "solid" ? "solid" : "outline",
+  };
+}
+
+export const boxIconsPack = definePack({
+  name: "BoxIcons",
+  prefix: "Bi",
+  variants: {
+    variant: ["outline", "solid"],
+  },
+  defaultVariants: {
+    variant: "outline",
+  },
+  download: {
+    zip: "https://github.com/atisawd/boxicons/archive/refs/heads/master.zip",
+    folder: "boxicons-master/svg",
+  },
+  contents: {
+    files: glob("download/MonoIcons/*.svg"),
+    extract: boxIconExtract,
+  },
+  projectUrl: "https://boxicons.com/",
+  license: "MIT",
+  licenseUrl: "https://github.com/atisawd/boxicons/blob/master/LICENSE",
+  coloring: "keep",
+  replaceColor: "#0D0D0D",
+});
